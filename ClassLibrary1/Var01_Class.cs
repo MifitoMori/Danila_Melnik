be7e0b41b11c03d375131976cd06714bf86322e5
Никита Сиротин <148621095+Blupup@users.noreply.github.com>
2024-05-02 14:20:49 +0300

    Update Var01_Class.cs

--- ClassLibrary1/Var01_Class.cs
+++ ClassLibrary1/Var01_Class.cs
@@ -61,14 +61,14 @@ namespace ClassLibrary1
         }
         public double Div(double n1, double n2)
         {
-            // Проверка деления на "0"
-            if (n2 == 0.0D)
-                throw new DivideByZeroException();
-            return n1 / n2;
+        // Проверка деления на "0"
+        if (n2 == 0.0D)
+            throw new DivideByZeroException();
+        return n1 / n2;
         }
         public double AddWithInc(double n1, double n2)
         {
-            return n1 + n2 + 1;
+        return n1 + n2 + 1;
         }
     }
 }
